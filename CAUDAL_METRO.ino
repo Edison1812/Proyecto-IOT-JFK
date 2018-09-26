8316c3ec4cd8191f9b10314fce88f281b0f90c26
Unknown <unknown@example.com>
2018-09-26 15:12:36 -0500

    Inicio

---
+++ CAUDAL_METRO.ino
@@ -0,0 +1,50 @@
+const int sensorPin = 2;
+const int measureInterval = 2500;
+volatile int pulseConter;
+ 
+// YF-S201
+const float factorK = 7.5;
+ 
+// FS300A
+//const float factorK = 5.5;
+ 
+// FS400A
+//const float factorK = 3.5;
+ 
+ 
+void ISRCountPulse()
+{
+   pulseConter++;
+}
+ 
+float GetFrequency()
+{
+   pulseConter = 0;
+ 
+   interrupts();
+   delay(measureInterval);
+   noInterrupts();
+ 
+   return (float)pulseConter * 1000 / measureInterval;
+}
+ 
+void setup()
+{
+   Serial.begin(9600);
+   attachInterrupt(digitalPinToInterrupt(sensorPin), ISRCountPulse, RISING);
+}
+ 
+void loop()
+{
+   // obtener frecuencia en Hz
+   float frequency = GetFrequency();
+ 
+   // calcular caudal L/min
+   float flow_Lmin = frequency * factorK;
+ 
+   Serial.print("Frecuencia: ");
+   Serial.print(frequency, 0);
+   Serial.print(" (Hz)\tCaudal: ");
+   Serial.print(flow_Lmin, 3);
+   Serial.println(" (L/min)");
+}
---
+++ proyecto_arduino.ino
@@ -0,0 +1,62 @@
+#include <SFE_BMP180.h>
+#include <Wire.h>
+SFE_BMP180 pressure;
+#define ALTITUDE 95.0
+int valor_limite=500 ;
+int rainsense= A2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(rainsense, INPUT); // Sensor de lluvia
+  pinMode(13,OUTPUT);      // Configura el Pin 13 como salida para el led 
+  pinMode(8,OUTPUT);      //Configura el Pin 8 como salida para el Zumbador 
+  pinMode(22,OUTPUT);
+  pinMode(11,OUTPUT);
+  if (pressure.begin()){}
+  else
+  {
+    while(1); // Pause forever.
+  }
+}
+
+void loop() {
+  char status;
+  double T,P,p0,a;
+  status = pressure.startTemperature();
+  status = pressure.getTemperature(T);
+  if (status != 0)
+  {
+    delay(status);
+    Serial.print(T,2);
+    Serial.print(" ");
+    delay(100);
+    status = pressure.getPressure(P,T);
+    delay(status);
+    Serial.print(P*0.0295333727,2);
+    Serial.print(" ");
+    delay(100);
+  }
+
+  Serial.print(analogRead(A0));    //Envia al Serial el valor leido del Sensor]
+  Serial.print(" ");
+  Serial.print(analogRead(A1));    //Envia al Serial el valor leido del Sensor] 
+  Serial.print(" ");
+  Serial.println(analogRead(rainsense));    //Envia al Serial el valor leido del Sensor] 
+   
+  if(analogRead(A0) < valor_limite or analogRead(A1) < valor_limite or  analogRead(A7) < 50) {  //Si la medida de humedad es mayor de valor limite
+    digitalWrite(13, HIGH) ;           //Enciende el led conectado al Pin 13 
+    digitalWrite(8, HIGH) ;           //Enciende el zumbador conectado al Pin 8 
+  }
+  else{                           //Si es mayor del valor limite apaga el zumbador y el led
+  digitalWrite(13,LOW);
+  digitalWrite(8,LOW);
+  
+  }
+  if(analogRead(rainsense)<500){
+    digitalWrite(11, HIGH) ; 
+    }
+  else {
+    digitalWrite(11, LOW) ; 
+    }
+  delay(250);
+}

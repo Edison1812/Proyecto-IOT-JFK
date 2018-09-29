e5be96396f78d6aea144605326749454f57e7a9e
JAVA <jose.avalos@utec.edu.pe>
2018-09-29 00:45:07 -0500

    Add web

---
+++ web/routes/dashboard.js
@@ -0,0 +1,22 @@
+/*******************************************************************************
+* Copyright (c) 2014 IBM Corporation and other Contributors.
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* IBM - Initial Contribution
+*******************************************************************************/
+
+var express = require('express');
+var router = express.Router();
+
+/* GET realtime page. */
+router.get('/', function(req, res) {
+	res.render('dashboard', { title: 'CH-IOT: Plataforma IOT para los recursos hidricos en CHincha' });
+});
+
+
+module.exports = router;
